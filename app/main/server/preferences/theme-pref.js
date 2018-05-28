c392e30d585257ab093085c5b89b1ac9ef523a16
Danny Allen <me@dannya.com>
2018-05-28 13:27:53 +0100

    * Enable transparency default settings based on known platform support (ie. on by default on MacOS, off by default on Windows)

--- app/main/server/preferences/theme-pref.js
+++ app/main/server/preferences/theme-pref.js
@@ -1,9 +1,9 @@
 'use strict';
 
+const conf = require('../../conf');
 const PreferencesObject = require('../../shared/preferences-object');
 const SimpleStore = require('../../shared/simple-store');
 const ThemeService = require('../app/theme-service');
-const conf = require('../../conf');
 
 const themePrefStore = new SimpleStore(conf.THEME_PREF_DIR);
 const themePrefSchema = require('./theme-pref-schema');
@@ -16,7 +16,7 @@ fse.ensureDirSync(conf.THEME_REPO);
 const themeService = new ThemeService();
 themeService.loadThemes();
 
-// set list of loaded themes into the preferences UI control
+// iterate through parsed theme objects, and add their names into an array if the theme is valid
 const userThemes = [];
 
 for (const i in themeService.userThemesObjs) {
@@ -25,9 +25,17 @@ for (const i in themeService.userThemesObjs) {
   }
 }
 
+// set list of loaded themes into the preferences UI control
 themePrefSchema.properties.activeTheme.enum = themePrefSchema.properties.activeTheme.enum.concat(
   userThemes
 );
+
+// set "enable transparency" defaults based on platform
+if (!conf.SUPPORTED_PLATFORMS_TRANSPARENCY.includes(process.platform)) {
+  // if we are not on a known supported platform, set setting and UI description to more conservative (off by default) values
+  themePrefSchema.properties.enableTransparency.default = themePrefSchema.properties.enableTransparency.default_win;
+  themePrefSchema.properties.enableTransparency.title = themePrefSchema.properties.enableTransparency.title_win;
+}
 
 // prepend user themes location to field help message
 themePrefSchema.properties.activeTheme.help = `Simply drop supported theme files into <code><a href="file:///${
